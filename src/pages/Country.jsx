8068a78e5829f495d9daa1604bd74fa33c084ee0
TaekyungAn <atk0426@naver.com>
2024-04-10 00:34:26 +0900

    react router 연습

---
+++ src/pages/Country.jsx
@@ -0,0 +1,7 @@
+import { useParams } from "react-router-dom";
+
+export default function Country() {
+  const params = useParams();
+  console.log(params);
+  return <div>Country : {params.code}</div>;
+}
