b750371168bacd9bfb2b925187a6da16400a81cb
TaekyungAn <atk0426@naver.com>
2024-06-10 20:08:49 +0900

    alt 속성 추가

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -16,7 +16,11 @@ export default function CountryItem({
   };
   return (
     <div onClick={onClickItem} className={style.container}>
-      <img className={style.flag_img} src={flagImg} />
+      <img
+        className={style.flag_img}
+        src={flagImg}
+        alt={`${commonName}의 국기 이미지입니다.`}
+      />
       <div className={style.content}>
         <div className={style.name}>
           {flagEmoji} {commonName}
